7e4610a43bacfb085c7fb849f3c2f53245d6c9a7
Sergey-aw <sergey@berisamocat.ru>
2024-11-18 20:33:13 +0300

    initial commit nextjs project

---
+++ beri/next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://complex-interfaces-512888.framer.app/:path*',
+        basePath: false,
+      },
+    ];
+},
+};
+
+export default nextConfig;
